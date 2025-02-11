d57c57ed8016998ae9165665a92d8a11c5402b2c
legendairydude <nickyoung_97@live.com>
2025-02-11 16:48:10 -0330

    "Rough landing page". Add comfortaa font

---
+++ theme/fonts.js
@@ -0,0 +1,8 @@
+import { Comfortaa } from "next/font/google";
+
+export const comfortaaFont = Comfortaa({
+  variable: "--font-comforta",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+});
--- theme/theme.js
+++ theme/theme.js
@@ -1,7 +1,9 @@
 import colours from "./colours";
+import { comfortaaFont } from "./fonts";
 
 const theme = {
   colours,
+  comfortaaFont,
 };
 
 export default theme;
